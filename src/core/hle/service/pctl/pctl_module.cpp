a43e739902132a799c23871c52353d7a58dc78fe
Kryptuq <68452851+Kryptuq@users.noreply.github.com>
2021-11-05 19:11:42 +0100

    2189

--- src/core/hle/service/pctl/pctl_module.cpp
+++ src/core/hle/service/pctl/pctl_module.cpp
@@ -7,7 +7,6 @@
 #include "core/file_sys/control_metadata.h"
 #include "core/file_sys/patch_manager.h"
 #include "core/hle/ipc_helpers.h"
-#include "core/hle/kernel/k_process.h"
 #include "core/hle/service/pctl/pctl.h"
 #include "core/hle/service/pctl/pctl_module.h"
 
@@ -45,7 +44,7 @@ public:
             {1014, nullptr, "ConfirmPlayableApplicationVideoOld"},
             {1015, nullptr, "ConfirmPlayableApplicationVideo"},
             {1016, nullptr, "ConfirmShowNewsPermission"},
-            {1017, nullptr, "EndFreeCommunication"},
+            {1017, &IParentalControlService::EndFreeCommunication, "EndFreeCommunication"},
             {1018, &IParentalControlService::IsFreeCommunicationAvailable, "IsFreeCommunicationAvailable"},
             {1031, &IParentalControlService::IsRestrictionEnabled, "IsRestrictionEnabled"},
             {1032, nullptr, "GetSafetyLevel"},
@@ -189,7 +188,7 @@ private:
 
         // TODO(ogniK): Recovery flag initialization for pctl:r
 
-        const auto tid = system.CurrentProcess()->GetTitleID();
+        const auto tid = system.GetCurrentProcessProgramID();
         if (tid != 0) {
             const FileSys::PatchManager pm{tid, system.GetFileSystemController(),
                                            system.GetContentProvider()};
@@ -232,6 +231,13 @@ private:
     void ConfirmStereoVisionPermission(Kernel::HLERequestContext& ctx) {
         LOG_DEBUG(Service_PCTL, "called");
         states.stereo_vision = true;
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(ResultSuccess);
+    }
+
+    void EndFreeCommunication(Kernel::HLERequestContext& ctx) {
+        LOG_WARNING(Service_PCTL, "(STUBBED) called");
 
         IPC::ResponseBuilder rb{ctx, 2};
         rb.Push(ResultSuccess);
