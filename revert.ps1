edcc31de765417bf5b84096dfce599c8bd870c61
Kryptuq <68452851+Kryptuq@users.noreply.github.com>
2022-07-28 01:08:32 +0200

    2863

--- revert.ps1
+++ revert.ps1
@@ -1,2 +1,2 @@
-Move-Item -Path C:\actions-runner\_work\Yuzu-Early-Access-files\Yuzu-Early-Access-files\build\bin\yuzu-early-access -Destination -Path C:\Users\Sven\AppData\Local\yuzu\
+Move-Item -Path C:\actions-runner\_work\Yuzu-Early-Access-files\Yuzu-Early-Access-files\build\bin\yuzu-early-access -Destination C:\Users\Sven\AppData\Local\yuzu\
 Rename-Item C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access C:\Users\Sven\AppData\Local\yuzu\yuzu-windows-msvc-early-access
