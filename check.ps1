fe60878c30b313afd89071b6f49469b085ebdfc1
Kryptuq <68452851+Kryptuq@users.noreply.github.com>
2022-07-28 00:22:54 +0200

    2863

--- check.ps1
+++ check.ps1
@@ -1,5 +1,5 @@
 Rename-Item C:\Users\Sven\AppData\Local\yuzu\yuzu-windows-msvc-early-access C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access
 
-Compress-Archive -LiteralPath "C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access" -DestinationPath "C:\Users\Sven\Desktop\yuzu-early-access.zip"
+Compress-Archive -LiteralPath "C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access" -DestinationPath "C:\Users\Sven\Desktop\yuzu-early-access-EA{{ github.event.head_commit.message }}.zip"
 
 Rename-Item C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access C:\Users\Sven\AppData\Local\yuzu\yuzu-windows-msvc-early-access
