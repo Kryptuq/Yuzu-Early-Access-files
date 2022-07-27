b67b19c0bfc0bb0cf872b92d7daad35074eac1d5
Kryptuq <68452851+Kryptuq@users.noreply.github.com>
2022-07-27 23:51:33 +0200

    test

---
+++ check.ps1
@@ -0,0 +1,5 @@
+Rename-Item C:\Users\Sven\AppData\Local\yuzu\yuzu-windows-msvc-early-access C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access
+
+Compress-Archive -LiteralPath "C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access" -DestinationPath "C:\Users\Sven\Desktop\yuzu-early-access.zip"
+
+Rename-Item C:\Users\Sven\AppData\Local\yuzu\yuzu-early-access C:\Users\Sven\AppData\Local\yuzu\yuzu-windows-msvc-early-access
